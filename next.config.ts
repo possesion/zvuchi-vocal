3d1b1ca3eaba389b40783854b94411cb4a040db0
Gennadiy K <possesion193@gmail.com>
2025-09-02 00:02:47 +0300

    конфиг

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  allowedDevOrigins: ["*"],
   /* config options here */
   images: {
     domains: ['drive.google.com', 'photos.google.com'],
