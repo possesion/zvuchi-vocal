3c1e931a8fd379bde0cd5d2a9ad61d1f17cad754
Gennadiy K <possesion193@gmail.com>
2026-08-27 00:20:52 +0300

    множитель

--- src/app/programs/constants.ts
+++ src/app/programs/constants.ts
@@ -5,5 +5,5 @@ export const LevelMultipliers = {
 
 export const MentorLevel = {
   expert: { value: 'expert', title: 'Эксперт', multiplier: 1.0 },
-  master: { value: 'master', title: 'Мастер', multiplier: 1.4 }
+  master: { value: 'master', title: 'Мастер', multiplier: 1.3 }
 } as const;
