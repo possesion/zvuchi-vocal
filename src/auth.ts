a5bce02d75c5e3ba73a7cfd530a4a13c10ef9fb6
Gennadiy K <possesion193@gmail.com>
2026-09-09 00:29:00 +0300

    scope yandex

--- src/auth.ts
+++ src/auth.ts
@@ -118,6 +118,11 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
         YandexProvider({
             clientId: process.env.YANDEX_CLIENT_ID,
             clientSecret: process.env.YANDEX_CLIENT_SECRET,
+            authorization: {
+                params: {
+                    scope: 'login:info login:email login:avatar login:default_phone',
+                },
+            },
             profile(profile) {
                 const { phone, avatarUrl } = extractYandexProviderData(profile)
                 return {
